3d40b303fcb2e4ba7a21a5de47c68d4633990afc
m4d <julien.conforto@gmail.com>
2022-12-29 17:17:42 +0100

    working dragbox

--- src/components/DragBox.js
+++ src/components/DragBox.js
@@ -1,7 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Matter, { Body } from "matter-js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Matter from 'matter-js';
+import { sample } from 'lodash';
 import { Card } from '@material-tailwind/react';
+import image1 from '../assets/ethWhite.png';
+import image2 from '../assets/jsLogo.jpg';
+import image3 from '../assets/blender.png';
+import image4 from '../assets/Ps.jpg';
+import image5 from '../assets/reactLogo.jpg';
+import image6 from '../assets/solidity.png';
 
 class Scene extends React.Component {
   constructor(props) {
@@ -15,138 +22,148 @@ class Scene extends React.Component {
       World = Matter.World,
       Bodies = Matter.Bodies,
       Mouse = Matter.Mouse,
-      MouseConstraint = Matter.MouseConstraint;
+      MouseConstraint = Matter.MouseConstraint,
+      Composites = Matter.Composites;
 
     var engine = Engine.create({
       // positionIterations: 20
     });
 
+    var engine = Engine.create({
+      gravity: {
+        x: 0,
+        y: 0.45,
+      },
+    });
+
     var render = Render.create({
       element: this.refs.scene,
       engine: engine,
+
       options: {
         width: 388,
-        height: 460,
-        pixelRatio: 2,
-        wireframes: false
+        height: 490,
+        background: 'black',
+        wireframes: false,
+      },
+    });
+
+    var images = [image1, image2, image3, image4, image5];
+
+    var ballA = Bodies.circle(render.options.width / 2, render.options.height / 2 - 100, 30, {
+      restitution: 0.5,
+      render: {
+        sprite: {
+          texture: image1,
+          xScale: 0.035,
+          yScale: 0.035,
+        },
+      },
+    });
+
+    var ballB = Bodies.circle(render.options.width / 2, render.options.height / 2 - 75, 35, {
+      restitution: 0.5,
+      render: {
+        sprite: {
+          texture: image3,
+          xScale: 0.2,
+          yScale: 0.2,
+        },
+      },
+    });
+
+    var ballC = Bodies.rectangle(render.options.width / 2, render.options.height / 2 - 50, 65, 65, {
+      restitution: 0.5,
+      render: {
+        sprite: {
+          texture: image2,
+          xScale: 0.15,
+          yScale: 0.15,
+        },
+      },
+    });
+
+    var ballD = Bodies.rectangle(render.options.width / 2, render.options.height / 2, 65, 65, {
+      restitution: 0.5,
+      render: {
+        sprite: {
+          texture: image4,
+          xScale: 0.04,
+          yScale: 0.04,
+        },
+      },
+    });
+
+    var ballE = Bodies.circle(
+      render.options.width / 2,
+      render.options.height / 2 + 25,
+      40,
+
+      {
+        restitution: 0.5,
+        render: {
+          sprite: {
+            texture: image5,
+            xScale: 0.03,
+            yScale: 0.03,
+          },
+        },
       }
-    });
-
-    var ground = Bodies.rectangle(
-      window.innerWidth / 2 + 160,
-      window.innerHeight + 80,
-      window.innerWidth + 320,
-      160,
-      { render: { fillStyle: "#080808" }, isStatic: true }
-    );
-    var wallLeft = Bodies.rectangle(
-      -80,
-      window.innerHeight / 2,
-      160,
-      window.innerHeight,
-      { isStatic: true }
-    );
-    var wallRight = Bodies.rectangle(
-      window.innerWidth + 80,
-      window.innerHeight / 2,
-      160,
-      1200,
-      { isStatic: true }
-    );
-    var roof = Bodies.rectangle(
-      window.innerWidth / 2 + 160,
-      -80,
-      window.innerWidth + 320,
-      160,
-      { isStatic: true }
     );
 
-    var shape1 = Bodies.circle(10, 595, 20, {
-      render: { fillStyle: "#7C3A2E" }
-    });
+    var ballF = Bodies.circle(
+      render.options.width / 2,
+      render.options.height / 2 + 50,
+      20,
 
-    var shape2 = Bodies.rectangle(100, 595, 100, 100, {
-      render: { fillStyle: "#C25822" }
-    });
-    var shape3 = Bodies.circle(200, 595, 40, {
-      render: { fillStyle: "#7C3A2E" }
-    });
-    var shape4 = Bodies.rectangle(280, 500, 80, 80, {
-      render: { fillStyle: "#ECD7C9" }
-    });
-    var shape5 = Bodies.rectangle(340, 595, 40, 40, {
-      render: { fillStyle: "#7C3A2E" }
-    });
-    var shape6 = Bodies.circle(360, 595, 30, {
-      render: { fillStyle: "#C25822" }
-    });
+      {
+        restitution: 0.5,
+        render: {
+          sprite: {
+            texture: image6,
+            xScale: 0.35,
+            yScale: 0.4,
+          },
+        },
+      }
+    );
 
     World.add(engine.world, [
-      ground,
-      wallLeft,
-      wallRight,
-      roof,
-      shape1,
-      shape2,
-      shape3,
-      shape4,
-      shape5,
-      shape6
-    ]);
-     World.add(engine.world, [
       //  walls
       // left
-       Bodies.rectangle(0, 0, 20, 600, { isStatic: true }),
-     //  right
-       Bodies.rectangle(400, 300, 20, 600, { isStatic: true }),
-     //  bottom
-       Bodies.rectangle(10, 600, 900, 10, { isStatic: true })
-     ]);
+      Bodies.rectangle(-10, 230, 5, 460, { isStatic: true, render: { fillStyle: '#000000' } }),
+      //  right
+      Bodies.rectangle(400, 230, 40, 460, { isStatic: true, render: { fillStyle: '#000000' } }),
+      //  bottom
+      Bodies.rectangle(180, 475, 388, 10, { isStatic: true, render: { fillStyle: '#000000' } }),
+      Bodies.rectangle(180, -10, 388, 10, { isStatic: true, render: { fillStyle: '#000000' } }),
+    ]);
 
-     World.add(engine.world, [
-       Bodies.circle(10, 225, 20, {
-         restitution: 0,
-         render: { fillStyle: "#7C3A2E" }
-       }),
-       Bodies.rectangle(100, 225, 100, 100, {
-         restitution: 0,
-         render: { fillStyle: "#C25822" }
-       }),
-       Bodies.circle(100, 255, 40, {
-         restitution: 0,
-         render: { fillStyle: "#7C3A2E" }
-       }),
-       Bodies.rectangle(180, 220, 80, 80, {
-         restitution: 0,
-         render: { fillStyle: "#ECD7C9" }
-       }),
-       Bodies.rectangle(240, 225, 40, 40, {
-         restitution: 0,
-         render: { fillStyle: "#7C3A2E" }
-       }),
-       Bodies.circle(260, 225, 30, {
-         restitution: 0,
-         render: { fillStyle: "#C25822" }
-       })
-     ]);
+    World.add(engine.world, ballA);
+    World.add(engine.world, ballB);
+    World.add(engine.world, ballC);
+    World.add(engine.world, ballD);
+    World.add(engine.world, ballE);
+    World.add(engine.world, ballF);
 
-    // add mouse control
     var mouse = Mouse.create(render.canvas),
       mouseConstraint = MouseConstraint.create(engine, {
         mouse: mouse,
         constraint: {
           stiffness: 0.2,
           render: {
-            visible: false
-          }
-        }
+            visible: false,
+            cursor: 'move',
+          },
+        },
+        element: null, // remove the mouseDown event listener
       });
 
     World.add(engine.world, mouseConstraint);
 
-    // Matter.Events.on(mouseConstraint, "mousedown", function (event) {
-    //   World.add(engine.world, Bodies.circle(150, 50, 30, { restitution: 0.7 }));
-    // });
+    Matter.Events.on(mouseConstraint, 'mousedown', function (event) {});
+
+    Matter.Events.off(mouseConstraint, 'mousedown');
 
     Engine.run(engine);
 
@@ -155,12 +172,10 @@ class Scene extends React.Component {
 
   render() {
     return (
-      <Card
-      className="border-solid border-zinc-200 border-[8px] rounded-3xl h-[476px] w-[388px] relative overflow-hidden will-change-transform"
-    >
-    <div ref="scene" />;
-    </Card>
-    )
+      <Card className="border-solid border-slate-900 border-[1px] rounded-3xl h-[476px] w-[388px] relative overflow-hidden will-change-transform">
+        <div ref="scene" />;
+      </Card>
+    );
   }
 }
 export default Scene;
